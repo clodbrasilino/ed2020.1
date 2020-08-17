cc51759daf300e2ba1de877d2ebd3f4b13c13423
Clodoaldo Brasilino <886522+clodbrasilino@users.noreply.github.com>
2020-08-17 10:29:09 -0300

    método de remoção

--- src/lista.js
+++ src/lista.js
@@ -33,6 +33,43 @@ class Lista {
         }
     }
 
+    buscarPelaPosicao(posicaoBuscada){
+        // navegação
+        let atual = this.cabeca;
+        let posicaoAtual = 1;
+        let elementoEncontrado = null;
+        while (atual != null && posicaoAtual != posicaoBuscada) {
+            atual = atual.proximo;
+            ++posicaoAtual;
+        }
+        // ao encontrar, retornar o elemento
+        if(atual != null) return atual.valor;
+        // é necessário ter cuidado com a ausência do elemento
+        return null;
+    }
+
+    removerPelaPosicao(posicaoASerApagada){
+        // navegação
+        let atual = this.cabeca;
+        let posicaoAtual = 1;
+
+        /* é necessário não perder a referência do elemento 
+         anterior ao elemento que vai ser apagado */
+
+        // 1º caso: o elemento a ser apagado é o primeiro
+        if(posicaoASerApagada == 1){
+            this.cabeca = this.cabeca.proximo;
+        }
+        // 2º caso: o elemento a ser apagado é o segundo ou posterior
+        else {
+            while(atual != null && posicaoAtual != posicaoASerApagada-1){
+                atual = atual.proximo;
+                ++posicaoAtual;
+            }
+            atual.proximo = atual.proximo.proximo;
+        }
+    }
+
     imprimir(){
         let atual = this.cabeca;
         let posicao = 1;
@@ -42,3 +79,15 @@ class Lista {
         }
     }
 }
+
+alunos = new Lista();
+alunos.inserirNoFim(new No('Tiago'));
+alunos.inserirNoFim(new No('Eduardo'));
+alunos.inserirNoFim(new No('Eduarda'));
+alunos.inserirNoFim(new No('Aparecida'));
+alunos.inserirNoFim(new No('Taciane'));
+alunos.inserirNoFim(new No('John'));
+alunos.inserirNoFim(new No('João'));
+alunos.imprimir();
+alunos.removerPelaPosicao(5);
+alunos.imprimir();
